40181580f536a1b90718a7bc09be6ac3e8faefff
Akil Anthony <akilanthony19@gmail.com>
2025-11-12 13:58:02 -0500

    Edit test for tdt_power_from_ET_ENT()

--- tests/testthat/test-tdt_power_from_ET_ENT.R
+++ tests/testthat/test-tdt_power_from_ET_ENT.R
@@ -1,6 +1,6 @@
 test_that("TDT power function runs without error", {
   res <- tdt_power_from_ET_ENT(ET = 140, ENT = 100, alpha = 0.05)
   expect_type(res, "list")
-  expect_true("Power" %in% names(res))  # Capital P
+  expect_true("Power" %in% names(res))
 })
 
