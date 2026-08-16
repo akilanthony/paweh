7b272dd7e50e94040d8b35f85120d005996bd1d7
Akil Anthony <akilanthony19@gmail.com>
2026-08-16 15:20:44 -0400

    Generalize TDT 3D surface visualization

--- tests/testthat/test-tdt-renamed-plots.R
+++ tests/testthat/test-tdt-renamed-plots.R
@@ -28,35 +28,3 @@ test_that("renamed specialized TDT 2D plots use the canonical backends", {
   expect_s3_class(mssn_misclassification, "ggplot")
   expect_s3_class(mssn_heterogeneity, "ggplot")
 })
-
-
-test_that("renamed transitional TDT 3D plots preserve Plotly output", {
-  skip_if_not_installed("plotly")
-
-  common <- list(
-    pd_seq = c(0.2, 0.3), prev = 0.05, R1 = 1.5, R2 = 2.25,
-    alpha = 0.05, delta_prime = 1
-  )
-
-  power_misclassification <- suppressMessages(do.call(
-    plot_tdt_power_phenotype_misclassification_3d,
-    c(common, list(N = 300, misclass_seq = c(0, 0.02)))
-  ))
-  power_heterogeneity <- suppressMessages(do.call(
-    plot_tdt_power_locus_heterogeneity_3d,
-    c(common, list(N = 300, heter_seq = c(0, 0.1)))
-  ))
-  mssn_misclassification <- suppressMessages(do.call(
-    plot_tdt_mssn_phenotype_misclassification_3d,
-    c(common, list(target_power = 0.8, misclass_seq = c(0, 0.02)))
-  ))
-  mssn_heterogeneity <- suppressMessages(do.call(
-    plot_tdt_mssn_locus_heterogeneity_3d,
-    c(common, list(target_power = 0.8, heter_seq = c(0, 0.1)))
-  ))
-
-  expect_s3_class(power_misclassification, "plotly")
-  expect_s3_class(power_heterogeneity, "plotly")
-  expect_s3_class(mssn_misclassification, "plotly")
-  expect_s3_class(mssn_heterogeneity, "plotly")
-})
